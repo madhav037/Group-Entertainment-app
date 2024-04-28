b0e34401fa9bbd31a65778e2f0d01c961183191e
madhav <109799001+madhav037@users.noreply.github.com>
2024-04-28 16:19:17 +0530

    google done

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -69,3 +69,60 @@ export const signin = async (req,res,next) => {
     .status(200)
     .json(user)
 }
+
+export const google = async (req,res,next) => {
+    try {
+        const { data, error } = await supabase
+        .from('user')
+        .select('*')
+        .eq('email', req.body.email)
+        .single()
+
+        if (error) {
+            res.status(500).json({"google error" : error})
+            return;
+        }
+
+        if (data) {
+            const token = jwt.sign({id : data.id}, process.env.JWT_SECRET)
+            const { password: pass, ...user } = data
+            res.cookie("access_token", token, {
+                httpOnly: true
+            })
+            .status(200)
+            .json(user)
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8)
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10)
+            const { data: user, error: err } = await supabase
+            .from('user')
+            .insert({ name: req.body.name.split(" ").join("").toLowerCase() +
+            Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, profile_picture: req.body.photo })
+            .select()
+
+            if (err) {
+                res.status(500).json({"google error" : err})
+                return;
+            }               
+
+            const token = jwt.sign({id : user.id}, process.env.JWT_SECRET)
+            const { password: pass, ...newUser } = user
+            res.cookie("access_token", token, {
+                httpOnly: true
+            })
+            .status(200)
+            .json(newUser)
+        }
+    } catch (error) {
+        res.status(500).json({"google error" : error})
+    }
+}
+
+export const signout = async (req,res,next) => {
+    try {
+        res.clearCookie("access_token")
+        res.status(200).send("signout successful")
+    } catch (error) {
+        res.status(500).json({"signout error" : error})
+    }
+}
