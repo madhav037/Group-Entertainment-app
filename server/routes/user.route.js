27b4a676e51b2b93537a6943beb636e1aa93ea64
madhav <109799001+madhav037@users.noreply.github.com>
2024-05-08 19:46:11 +0530

    commited?

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,5 @@
 import express from 'express';
-<<<<<<< HEAD
 import { addFriend, changePassword, getCurrentUser, getProfilePicture, uploadProfilePicture } from '../controllers/user.controller.js';
-=======
-import { addFriend, changePassword, getProfilePicture, getUser, uploadProfilePicture } from '../controllers/user.controller.js';
->>>>>>> 3fae3a08f8b98f8c29ee677bce12380b8a2bf4f9
 import multer from 'multer';
 import { google } from '../controllers/auth.controller.js';
 const router = express.Router()
